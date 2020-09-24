711aa59d9af6292e6b5c20c5bfae923545a4772a
olivier.bonheur <olivier.bonheur@gmail.com>
2020-09-24 23:41:14 +0200

    Work on Reward module Controller

--- TourGuide/src/main/java/rewardModule/controller/RewardController.java
+++ TourGuide/src/main/java/rewardModule/controller/RewardController.java
@@ -1,6 +1,8 @@
 package rewardModule.controller;
 
 import com.jsoniter.output.JsonStream;
+import gpsUtil.location.Attraction;
+import gpsUtil.location.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,5 +18,15 @@ public class RewardController {
     @RequestMapping("/getPrice")
     public void calculateRewards(User user) {
         rewardsService.calculateRewards(user);
+    }
+
+    @RequestMapping("/getRewardPoints")
+    public String getRewardPoints(Attraction attraction, User user) {
+        return JsonStream.serialize(rewardsService.getRewardPoints(attraction, user));
+    }
+
+    @RequestMapping("/getDistance")
+    public String getDistance(Location loc1, Location loc2) {
+        return JsonStream.serialize(rewardsService.getDistance(loc1, loc2));
     }
 }
