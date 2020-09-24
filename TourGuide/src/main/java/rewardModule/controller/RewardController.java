e05086ac7ef66eaff24c5c9531f3214374db65a0
olivier.bonheur <olivier.bonheur@gmail.com>
2020-09-24 22:54:31 +0200

    Work on Reward module Controller

--- TourGuide/src/main/java/rewardModule/controller/RewardController.java
+++ TourGuide/src/main/java/rewardModule/controller/RewardController.java
@@ -1,4 +1,20 @@
 package rewardModule.controller;
 
+import com.jsoniter.output.JsonStream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import rewardModule.service.IRewardsService;
+import tourGuide.domain.User;
+
+@RestController
 public class RewardController {
+
+    @Autowired
+    IRewardsService rewardsService;
+
+    @RequestMapping("/getPrice")
+    public void calculateRewards(User user) {
+        rewardsService.calculateRewards(user);
+    }
 }
