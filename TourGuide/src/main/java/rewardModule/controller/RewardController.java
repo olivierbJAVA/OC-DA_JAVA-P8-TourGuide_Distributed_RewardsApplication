f1b1caafba03626652e1fe70cfe571c03f3ce53c
olivier.bonheur <olivier.bonheur@gmail.com>
2020-09-27 00:40:33 +0200

    TourGuide_Rewards-Application initial version

--- TourGuide/src/main/java/rewardModule/controller/RewardController.java
+++ TourGuide/src/main/java/rewardModule/controller/RewardController.java
@@ -1,7 +1,5 @@
 package rewardModule.controller;
 
-import com.jsoniter.output.JsonStream;
-import gpsUtil.location.Attraction;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
