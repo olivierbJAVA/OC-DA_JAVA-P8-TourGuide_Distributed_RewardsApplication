9afcfd9a90066e8e22fba62d5e46f2cf00696094
olivier.bonheur <olivier.bonheur@gmail.com>
2020-09-27 22:29:16 +0200

    Working on Controller

--- TourGuide/src/main/java/rewardModule/controller/RewardController.java
+++ TourGuide/src/main/java/rewardModule/controller/RewardController.java
@@ -1,5 +1,7 @@
 package rewardModule.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,13 +12,16 @@ import java.util.UUID;
 
 @RestController
 public class RewardController {
+    private Logger logger = LoggerFactory.getLogger(RewardController.class);
 
     @Autowired
     IRewardsService rewardsService;
 
     @RequestMapping("/getRewardPoints")
-    public int getRewardPoints(@RequestParam UUID attractionId, @RequestParam UUID userId) {
-        return rewardsService.getRewardPoints(attractionId, userId);
+    public int getRewardPoints(@RequestParam String attractionId, @RequestParam String userId) {
+        logger.debug("Request getRewardPoints");
+        int rewardsPoints = rewardsService.getRewardPoints(UUID.fromString(attractionId), UUID.fromString(userId));
+        logger.debug("Response rewardsPoints=" + rewardsPoints);
+        return rewardsPoints;
     }
-
 }
